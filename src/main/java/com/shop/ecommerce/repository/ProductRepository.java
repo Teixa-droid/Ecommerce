a3f49df2e6945db1a37806a3e6c5b6b32b1001ef
Teixa-droid <ruben.a.teixa@gmail.com>
2022-02-16 02:42:37 +0000

    create the repository and the classes and show method of ProductoController

---
+++ src/main/java/com/shop/ecommerce/repository/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.shop.ecommerce.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.shop.ecommerce.model.Product;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+	
+}
