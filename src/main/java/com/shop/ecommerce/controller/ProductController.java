17e00278192ad7080cdd4c78b8bdbb8ff44704f5
Teixa-droid <ruben.a.teixa@gmail.com>
2022-02-16 20:40:19 +0000

    functionality to delete a product record

--- src/main/java/com/shop/ecommerce/controller/ProductController.java
+++ src/main/java/com/shop/ecommerce/controller/ProductController.java
@@ -15,49 +15,55 @@ import com.shop.ecommerce.model.Product;
 import com.shop.ecommerce.model.User;
 import com.shop.ecommerce.service.ProductService;
 
-
 @Controller
 @RequestMapping("/products")
 public class ProductController {
-private final Logger LOGGER = LoggerFactory.getLogger(ProductController.class);
-	
+	private final Logger LOGGER = LoggerFactory.getLogger(ProductController.class);
+
 	@Autowired
 	private ProductService productService;
-	
+
 	@GetMapping("")
 	public String show(Model model) {
 		model.addAttribute("products", productService.findAll());
 		return "products/show";
 	}
-	
+
 	@GetMapping("/create")
 	public String create() {
 		return "products/create";
 	}
-	
+
 	@PostMapping("/save")
 	public String save(Product product) {
-		LOGGER.info("Este é o objeto do produto {}",product);
-		User u= new User(1, "", "", "", "", "", "", "");
-		product.setUser(u);		
+		LOGGER.info("Este é o objeto do produto {}", product);
+		User u = new User(1, "", "", "", "", "", "", "");
+		product.setUser(u);
 		productService.save(product);
 		return "redirect:/products";
 	}
+
 	@GetMapping("/edit/{id}")
 	public String edit(@PathVariable Integer id, Model model) {
-		Product product= new Product();
-		Optional<Product> optionalProduct=productService.get(id);
-		product= optionalProduct.get();
+		Product product = new Product();
+		Optional<Product> optionalProduct = productService.get(id);
+		product = optionalProduct.get();
 
-		LOGGER.info("Produto pesquisado: {}",product);
+		LOGGER.info("Produto pesquisado: {}", product);
 		model.addAttribute("product", product);
 
 		return "products/edit";
 	}
 
 	@PostMapping("/update")
-	public String update(Product product ) {
+	public String update(Product product) {
 		productService.update(product);
+		return "redirect:/products";
+	}
+
+	@GetMapping("/delete/{id}")
+	public String delete(@PathVariable Integer id) {
+		productService.delete(id);
 		return "redirect:/products";
 	}
 
