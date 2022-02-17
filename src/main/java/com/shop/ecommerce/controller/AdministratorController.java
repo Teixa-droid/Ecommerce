8e2b8fe9a0df6a763b0239c9a26c6d2e4e67c0e6
Teixa-droid <ruben.a.teixa@gmail.com>
2022-02-17 00:46:25 +0000

    implement image upload and image display functionalities for the administrator

--- src/main/java/com/shop/ecommerce/controller/AdministratorController.java
+++ src/main/java/com/shop/ecommerce/controller/AdministratorController.java
@@ -1,15 +1,26 @@
 package com.shop.ecommerce.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.shop.ecommerce.model.Product;
+import com.shop.ecommerce.service.ProductService;
 
 @Controller
 @RequestMapping("/administrator")
 public class AdministratorController {
+	@Autowired
+	private ProductService productService;
 	
 	@GetMapping("")
-	public String home() {
+	public String home(Model model) {
+		List<Product> products=productService.findAll();
+		model.addAttribute("products", products);
 		return "administrator/home";
 	}
 
--- src/main/java/com/shop/ecommerce/controller/ProductController.java
+++ src/main/java/com/shop/ecommerce/controller/ProductController.java
@@ -22,71 +22,87 @@ import com.shop.ecommerce.service.UploadFileService;
 @Controller
 @RequestMapping("/products")
 public class ProductController {
-	
+
 	private final Logger LOGGER = LoggerFactory.getLogger(ProductController.class);
-	
+
 	@Autowired
 	private ProductService productService;
-	
+
 	@Autowired
 	private UploadFileService upload;
-	
+
 	@GetMapping("")
 	public String show(Model model) {
 		model.addAttribute("products", productService.findAll());
 		return "products/show";
 	}
-	
+
 	@GetMapping("/create")
 	public String create() {
 		return "products/create";
 	}
-	
+
 	@PostMapping("/save")
 	public String save(Product product, @RequestParam("img") MultipartFile file) throws IOException {
-		LOGGER.info("Este é o objeto do produto {}",product);
-		User u= new User(1, "", "", "", "", "", "", "");
-		product.setUser(u);	
-		
-		//image
-		if (product.getId()==null) { 
-			String nameImage= upload.saveImage(file);
+		LOGGER.info("Este é o objeto do produto {}", product);
+		User u = new User(1, "", "", "", "", "", "", "");
+		product.setUser(u);
+
+		// image
+		if (product.getId() == null) {
+			String nameImage = upload.saveImage(file);
 			product.setImage(nameImage);
-		}else {
-			if (file.isEmpty()) {
-				Product p= new Product();
-				p=productService.get(product.getId()).get();
-				product.setImage(p.getImage());
-			}
+		} else {
+
 		}
-		
+
 		productService.save(product);
 		return "redirect:/products";
 	}
-	
+
 	@GetMapping("/edit/{id}")
 	public String edit(@PathVariable Integer id, Model model) {
-		Product product= new Product();
-		Optional<Product> optionalProduct=productService.get(id);
-		product= optionalProduct.get();
-		
-		LOGGER.info("Product search: {}",product);
+		Product product = new Product();
+		Optional<Product> optionalProduct = productService.get(id);
+		product = optionalProduct.get();
+
+		LOGGER.info("Product search: {}", product);
 		model.addAttribute("product", product);
-		
+
 		return "products/edit";
 	}
-	
+
 	@PostMapping("/update")
-	public String update(Product product ) {
+	public String update(Product product, @RequestParam("img") MultipartFile file) throws IOException {
+		Product p = new Product();
+		p = productService.get(product.getId()).get();
+		if (file.isEmpty()) {
+			
+			product.setImage(p.getImage());
+		} else {
+
+			if (!p.getImage().equals("default.jpg")) {
+				upload.deleteImage(p.getImage());
+			}
+
+			String nameImage = upload.saveImage(file);
+			product.setImage(nameImage);
+		}
+		product.setUser(p.getUser());
 		productService.update(product);
 		return "redirect:/products";
 	}
-	
+
 	@GetMapping("/delete/{id}")
 	public String delete(@PathVariable Integer id) {
+		Product p = new Product();
+		p = productService.get(id).get();
+
+		if (!p.getImage().equals("default.jpg")) {
+			upload.deleteImage(p.getImage());
+		}
 		productService.delete(id);
 		return "redirect:/products";
 	}
-	
-	
+
 }
