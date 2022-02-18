75b8978ed55bbf467c14095c29dca8943d1dcb46
Teixa-droid <ruben.a.teixa@gmail.com>
2022-02-18 04:48:48 +0000

    method to generate order number

--- src/main/java/com/shop/ecommerce/service/IOrderService.java
+++ src/main/java/com/shop/ecommerce/service/IOrderService.java
@@ -1,8 +1,11 @@
 package com.shop.ecommerce.service;
 
+import java.util.List;
+
 import com.shop.ecommerce.model.Order;
 
 public interface IOrderService {
+	List<Order> findAll();
 	Order save (Order order);
 
 }
--- src/main/java/com/shop/ecommerce/service/OrderServiceImpl.java
+++ src/main/java/com/shop/ecommerce/service/OrderServiceImpl.java
@@ -1,5 +1,8 @@
 package com.shop.ecommerce.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +17,40 @@ public class OrderServiceImpl implements IOrderService {
 	@Override
 	public Order save(Order order) {
 		return orderRepository.save(order);
+	}
+	@Override
+	public List<Order> findAll() {
+		return orderRepository.findAll();
+	}
+	// 0000010
+	public String generateOrderNumber() {
+		int number=0;
+		String numberConcatenated="";
+
+		List<Order> orders = findAll();
+
+		List<Integer> numbers= new ArrayList<Integer>();
+
+		orders.stream().forEach(o -> numbers.add( Integer.parseInt( o.getNumber())));
+
+		if (orders.isEmpty()) {
+			number=1;
+		}else {
+			number= numbers.stream().max(Integer::compare).get();
+			number++;
+		}
+
+		if (number<10) { //0000001000
+			numberConcatenated="000000000"+String.valueOf(number);
+		}else if(number<100) {
+			numberConcatenated="00000000"+String.valueOf(number);
+		}else if(number<1000) {
+			numberConcatenated="0000000"+String.valueOf(number);
+		}else if(number<10000) {
+			numberConcatenated="0000000"+String.valueOf(number);
+		}		
+
+		return numberConcatenated;
 	}
 
 }
