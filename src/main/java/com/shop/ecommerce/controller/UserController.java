c389467648839d50c7effdd703e2e0fc8a1ae3e8
Teixa-droid <ruben.a.teixa@gmail.com>
2022-02-19 05:58:23 +0000

    show the shopping view for the user

--- src/main/java/com/shop/ecommerce/controller/UserController.java
+++ src/main/java/com/shop/ecommerce/controller/UserController.java
@@ -10,6 +10,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -62,6 +63,11 @@ public class UserController {
 		}
 
 		return "redirect:/";
+	}
+	@GetMapping("/shopping")
+	public String obtenerCompras(Model model, HttpSession session) {
+		model.addAttribute("session", session.getAttribute("iduser"));
+		return "user/shopping";
 	}
 
 }
